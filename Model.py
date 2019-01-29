a54080e6c34cac481611dd339c0a5ecf88b1cdff
alenagerlinskaja <ag146@hdm-stuttgart.de>
2019-01-30 00:58:56 +0100

    Weitere Imports eingefuegt & aufbau des Models eingefuegt. Sequenz & Kompilier Funktion von TF Keras eingebaut.

--- Model.py
+++ Model.py
@@ -1,3 +1,22 @@
 import tensorflow as tf
 from tensorflow import keras
 from Usage import Usage
+from Dataset import Dataset
+from Dictionary import DictionaryManager
+from Conversation import Conversation
+from DocumentBase import DocumentBase
+
+class Model():
+
+    def __init__(self):
+        pass
+
+    #Usage of keras.Sequential & compile function by tensorflow keras
+    def buildModel(self):
+        model = keras.Sequential([
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(7, activation=tf.nn.softmax)])
+        model.compile(optimizer=tf.train.AdamOptimizer(),
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        return model

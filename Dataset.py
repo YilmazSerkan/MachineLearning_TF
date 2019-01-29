ae0dc38266f3d725ccd31e524d6a3343ba0dd7f4
roxanadinus <rd028@hdm-stuttgart.de>
2019-01-30 00:21:18 +0100

    Train & Test Split Function & Normalisierung

--- Dataset.py
+++ Dataset.py
@@ -18,3 +18,13 @@ class Dataset:
       labels = pd.read_csv(dataObj)
       labels = np.array(labels).squeeze()
       return labels
+
+  #Train_test_split function by sklearn to split loaded data into train and test data by scope of 80/20
+  def splitTrainTestData(self, xval, yval):
+      xtrain, xtest, ytrain, ytest = train_test_split(xval, yval, test_size=0.2, random_state=10)
+      return xtrain, xtest, ytrain, ytest
+
+  #Normalize data values to a range of 0 to 1 for better neural network performance
+  def normalizeData(self, dataObj):
+      normalizedData = dataObj / 378.0
+      return normalizedData

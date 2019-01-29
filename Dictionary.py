333e0d0b38eb7180316569075f9aaf76f28a0f2c
roxanadinus <rd028@hdm-stuttgart.de>
2019-01-30 00:51:49 +0100

    Kernfunktion eingefuegt, De- und Encodieren der Repraeresentation von Keywords umgesetzt.

--- Dictionary.py
+++ Dictionary.py
@@ -13,3 +13,17 @@ class DictionaryManager:
     json_data = open(r"C:\Users\Serkan Yilmaz\PycharmProjects\MachineLearning_TF\dictkeywords.json").read()
     my_dict = json.loads(json_data)
     reverse_word_index = dict([(value, key) for (key, value) in my_dict.items()])
+
+    #decode number reprensetation of keywords into initial strings by using dictionary
+    def decode_keywords(self, dataObj):
+        return ' '.join([DictionaryManager.reverse_word_index.get(i, '?') for i in dataObj])
+
+    #encode string keywords into number reprensentation by using dictionary
+    def encode_keywords(self, dataObj):
+        newList = []
+        for i in range(len(dataObj)):
+            for j in range(len(dataObj[i])):
+                mykeys = dataObj[i]
+            newList.append([DictionaryManager.my_dict[k] for k in mykeys if k in DictionaryManager.my_dict])
+        textToNumList = np.asarray(newList)
+        return textToNumList

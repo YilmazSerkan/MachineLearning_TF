41828ce6d32fa8f11b9cebb030c24e4f0ecbfcb7
alenagerlinskaja <ag146@hdm-stuttgart.de>
2019-01-30 01:03:22 +0100

    Durchführung von zwei Trainings & zwei Tests für beste Modellleistung unter Verwendung verschiedener Datensätze.

--- Model.py
+++ Model.py
@@ -20,3 +20,23 @@ class Model():
                       loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
         return model
+
+    #Usage fit function by tf.keras for training
+    def trainModel(self,model, xtrain, ytrain):
+        #model = Model.buildModel()
+        model.fit(xtrain, ytrain, epochs=5)
+
+    #Usage of evaluate fuction by tf.keras for test
+    def testModel(self,model,xtest,ytest):
+        test_loss, test_acc = model.evaluate(xtest, ytest)
+        print('Test accuracy:', test_acc)
+
+    #Usage of predict fuction by tf.keras for prediction
+    def executePrediction(self, model, convesationObj):
+        predictions = model.predict(convesationObj)
+        return predictions
+
+"""Execution of two trainings & two tests for best model preformance by using different Datasets:
+   Dataset DB.csv --> Represents documents tagged with specific keywords
+   Dataset Usage.csv --> Represents keywords collected from past conversations
+   Execution of a prediction by using a dataset which simulates a conversation"""
